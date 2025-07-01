193233170283a19fb2e431fa979f797a6a7b895a
minniesBaa <julianjohnsontaylor@gmail.com>
2025-07-01 16:56:51 -0700

    update example code

--- examples/read_and_echo.py
+++ examples/read_and_echo.py
@@ -1,11 +1,11 @@
-import mouselink as sl
+import mouselink as sl # import mouselink
 
-p = sl.ev3()
+p = sl.ev3() # set "p" to an ev3 object
 
-def on_read(data):
-    print(f"Got data: {data}")
-    p.send(f"111{data}")
+def on_got_data(data): # function to handle recieved data
+    print(f"Got data: {data}") # post the data recieved to the console
+    p.send(data) # echo the data back
 
-p.on_read(on_read)
+p.on_read(on_got_data) # tell mouselink to call "on_got_data" whenever it gets data from Scratch
 
-p.run()
+p.run() # start mouselink
